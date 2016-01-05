36fbe9aebeb14a1a347462f87574ddbd28c8521a
hoangphanea <hoang.phan@eastagile.com>
2016-01-05 16:39:27 +0700

    [f] Custom camera app

--- app/src/main/java/vn/hoangphan/facerecognition/constants/Constants.java
+++ app/src/main/java/vn/hoangphan/facerecognition/constants/Constants.java
@@ -4,5 +4,5 @@ package vn.hoangphan.facerecognition.constants;
  * Created by Hoang Phan on 1/4/2016.
  */
 public interface Constants {
-    String API_ENDPOINT = "http://4b048c57.ngrok.com";
+    String API_ENDPOINT = "http://4a2fe80e.ngrok.com";
 }
